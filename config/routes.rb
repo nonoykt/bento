4403d29211d72227a7d97dbf0bf27dcbbfbc2a5f
hrkyjp <nonoykt@gmail.com>
2020-04-12 18:24:46 +0900

    Settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
+  root to: 'users#index'
+  resources :users
 end
