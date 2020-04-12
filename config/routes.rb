1d9d10f2cd67faf01156693289cf7c8a0bc4ef51
hrkyjp <nonoykt@gmail.com>
2020-04-13 00:01:45 +0900

    generate devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :shops
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-=======
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks'}
->>>>>>> genarating-devise
+  devise_for :shops,
+    path: 'shops',
+    controllers: {
+      sessions: 'shops/sessions',
+      registrations: 'shops/registrations'
+    }
+  resources :shops
+
+  devise_for :users,
+    path: 'users',
+    controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations',
+      omniauth_callbacks: 'users/omniauth_callbacks'
+    }
   root to: 'users#index'
   resources :users
 end
